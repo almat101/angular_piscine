471d58a67e8b77e797f353670883645ec389e12f
almat101 <sandruozzo@gmail.com>
2024-05-07 15:57:22 +0200

    add week2_day01/ex00
    first angular components

---
+++ week2_day01/ex00/Angular_Acute/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MyCardComponent } from './my-card/my-card.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+	RouterOutlet,
+	MyCardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Angular_Acute';
+}
